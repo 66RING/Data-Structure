dc944643273ebc06209b0459fb65fa96a5423aeb
66RING <travisturing@qq.com>
2022-10-05 11:08:30 +0800

    lru

---
+++ lru/README.md
@@ -0,0 +1,12 @@
+# LRU
+
+基于双向链表 + HashMap的LRU
+
+- 链表用于存储
+- HashMap用于对key索引
+
+## Result
+
+- Rust中HashMap的使用
+	* `get`
+- `Rc<RefCell<T>>`
---
+++ lru/src/main.rs
@@ -0,0 +1,189 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+struct LRUNode<T> {
+    next: Option<Rc<RefCell<LRUNode<T>>>>,
+    prev: Option<Rc<RefCell<LRUNode<T>>>>,
+    key: T,
+    value: T,
+}
+
+struct LRUCache {
+    head: Option<Rc<RefCell<LRUNode<i32>>>>,
+    map: HashMap<i32, Rc<RefCell<LRUNode<i32>>>>,
+    capacity: i32,
+    len: i32,
+}
+
+
+impl<T> LRUNode<T> {
+    fn new(key: T, value: T) -> Self {
+        Self { next: None, prev: None, key, value }
+    }
+}
+
+impl LRUCache {
+    fn new(capacity: i32) -> Self {
+        Self { head: None, map: HashMap::new(), capacity, len: 0 }
+    }
+    
+    // 使用hashmap索引查找
+    fn get(&mut self, key: i32) -> i32 {
+        // 如果存在则移到链首
+        if let Some(node) = self.map.get(&key) {
+            let v = node.borrow_mut().value;
+            self.move_to_head(node.clone());
+            return v;
+        } else {
+            return -1;
+        }
+    }
+    
+    fn put(&mut self, key: i32, value: i32) {
+        // 如果key已存在则覆盖, 并移动到链首
+        if let Some(node) = self.map.get(&key) {
+            node.borrow_mut().value = value;
+            self.move_to_head(node.clone());
+        } else {
+            // 不存在则在链首插入
+            if self.len == self.capacity {
+                let t = self.head.as_ref().unwrap().borrow_mut().prev.as_ref().unwrap().clone();
+                // 如果LRU缓存已满, 则删除链尾元素
+                //  1. 删除链表节点和哈希表节点
+                self.delete(t.clone());
+                //  2. 链首插入新元素, 包括接节点和hashmap
+                let node = Rc::new(RefCell::new(LRUNode::new(key, value)));
+                //      2.1 插入链首
+                self.move_to_head(node.clone());
+                //      2.2 插入hashmap
+                self.map.insert(key, node.clone());
+            } else {
+                self.len += 1;
+                // 如果LRU缓存未满
+                //  1. 链首插入新元素, 包括接节点和hashmap
+                let node = Rc::new(RefCell::new(LRUNode::new(key, value)));
+                //      1.1 插入链首
+                self.move_to_head(node.clone());
+                //      1.2 插入hashmap
+                self.map.insert(key, node.clone());
+            }
+        }
+    }
+
+    fn delete(&mut self, node: Rc<RefCell<LRUNode<i32>>>) {
+        self.map.remove(&node.borrow().key);
+        let prev = node.borrow_mut().prev.as_ref().unwrap().clone();
+        let next = node.borrow_mut().next.as_ref().unwrap().clone();
+        prev.borrow_mut().next = Some(next.clone());
+        next.borrow_mut().prev = Some(prev.clone());
+    }
+
+    fn move_to_head(&mut self, node: Rc<RefCell<LRUNode<i32>>>) {
+        if self.head.is_none() {
+            self.head = Some(node.clone());
+            node.borrow_mut().next = Some(node.clone());
+            node.borrow_mut().prev = Some(node.clone());
+        } else if  self.head.as_ref().unwrap().borrow().key == node.as_ref().borrow().key {
+            return;
+        } else {
+            // 插入新节点时: prev, next为空
+            if node.borrow_mut().prev.is_some() {
+                let prev = node.borrow_mut().prev.as_ref().unwrap().clone();
+                let next = node.borrow_mut().next.as_ref().unwrap().clone();
+                // 如果是不是新节点, 其prev和next将非空
+                // 断开连接
+                prev.borrow_mut().next = Some(next.clone());
+                next.borrow_mut().prev = Some(prev.clone());
+            }
+
+            // 移动到链首
+            let h = self.head.as_ref().unwrap().clone();
+            let t = self.head.as_ref().unwrap().borrow_mut().prev.as_ref().unwrap().clone();
+            // 与旧head相连
+            node.borrow_mut().next = Some(h.clone());
+            // 与旧tail相连
+            node.borrow_mut().prev = Some(t.clone());
+            h.borrow_mut().prev = Some(node.clone());
+            t.borrow_mut().next = Some(node.clone());
+            self.head = Some(node);
+        }
+    }
+
+    fn erase(&mut self) { }
+    fn pop_back(&mut self) { }
+    fn push_front(&mut self, node: Rc<RefCell<LRUNode<i32>>>) { }
+}
+
+
+#[test]
+fn test() {
+    // ["LRUCache","put","put","get","put","get",// "put","get","get","get"]
+    // [[2],[1,1],[2,2],[1],[3,3], // [2],[4,4],[1],[3],[4]]
+    let mut obj = LRUCache::new(2);
+    obj.put(1, 1);
+    obj.put(2, 2);
+    // 1 -> 2
+    assert_eq!(1, obj.get(1));
+    // 3 -> 1
+    obj.put(3, 3);
+    // should be -1 since lru cap = 2
+    assert_eq!(-1, obj.get(2));
+    // 4 -> 3
+    obj.put(4, 4);
+    assert_eq!(-1, obj.get(1));
+    assert_eq!(3, obj.get(3));
+    assert_eq!(4, obj.get(4));
+
+    // ["LRUCache","put","put","get",// "put","get","put","get","get","get"]
+    // [[2],[1,0],[2,2],[1]// ,[3,3],[2],[4,4],[1],[3],[4]]
+    let mut obj = LRUCache::new(2);
+    // 1:0 
+    obj.put(1, 0);
+    // 2:2 -> 1:0
+    obj.put(2, 2);
+    // 1:0 -> 2:2
+    assert_eq!(0, obj.get(1));
+    // 3:3 -> 1:0
+    obj.put(3, 3);
+    assert_eq!(-1, obj.get(2));
+    // 4:4 -> 3:3
+    obj.put(4, 4);
+    assert_eq!(-1, obj.get(1));
+    assert_eq!(3, obj.get(3));
+    assert_eq!(4, obj.get(4));
+
+    // ["LRUCache","put","get","put","get","get"]
+    // [[1],[2,1],[2],[3,2],[2],[3]]
+    let mut obj = LRUCache::new(1);
+    obj.put(2, 1);
+    assert_eq!(1, obj.get(2));
+    obj.put(3, 2);
+    assert_eq!(-1, obj.get(2));
+    assert_eq!(2, obj.get(3));
+
+    // ["LRUCache","put","put","get","get","put","get","get","get"]
+    // [[2],[2,1],[3,2],[3],[2],[4,3],[2],[3],[4]]
+    // 头节点被使用, rehead的情况
+    let mut obj = LRUCache::new(2);
+    // 2:1
+    obj.put(2, 1);
+    assert_eq!(1, obj.get(2));
+    // 3:2 -> 2:1
+    obj.put(3, 2);
+    assert_eq!(2, obj.get(3));
+    // 2:1 -> 3:2
+    assert_eq!(1, obj.get(2));
+    // 4:3 -> 2:1
+    obj.put(4, 3);
+    // 2:1 -> 4:3
+    assert_eq!(1, obj.get(2));
+    assert_eq!(-1, obj.get(3));
+    // 4:3 -> 2:1
+    assert_eq!(3, obj.get(4));
+}
+
+
+
+fn main() {
+}
